c661ab4c64e76ee2cec8fa4f27a4efdfa21c6246
Renan Francisco Souza <renan-francisco@hotmail.com>
2012-05-30 00:52:51 -0300

    Feito marcaQuartaLinha

--- js/src/Tabuleiro.js
+++ js/src/Tabuleiro.js
@@ -37,6 +37,25 @@ function cria_mapa_arestas_quadrados(_linhas, _colunas){
 	return [mapa_quadrados_arestas, mapa_arestas_quadrados];
 }
 
+function isEqual(v1,v2) {
+    if (v1.length != v2.length) return false;
+    for (var i = 0; i < v1.length; i++) {
+        if (v1[i] != v2[i])
+            return false;   
+    }
+    return true;
+}
+/*
+Array.prototype.isEqual = function(v) {
+    var len = this.length;  
+    for (var i = 0; i < len; i++) {
+        if (this[i] != v[i])
+            return false;       
+    }
+    return true;
+}
+*/
+
 function Tabuleiro(_linhas, _colunas, _marcadas, _quadradosJogador, _quadradosComputador){
 	this.linhas = _linhas;
 	this.colunas = _colunas;
@@ -125,6 +144,7 @@ Tabuleiro.prototype.addQuadradosComputador = function(novosQuadrados){
 }
 
 /**
+ * @param aresta O id da aresta
  * @return Retorna uma lista de duplas que definem os quadrados que tem a aresta parametro em sua formacao. 
  */
 Tabuleiro.prototype.getArestaQuadrados = function(aresta){ // getArestaQuadrados(1) [ [1,2], [2,1] ]
@@ -141,7 +161,7 @@ Tabuleiro.prototype.getQuadradoArestas = function(cordX, cordY){ // getQuadradoA
 Tabuleiro.prototype.quadradoEstaCompleto = function(cordX, cordY){
     var arestas = this.getQuadradoArestas(cordX, cordY);
     var countPreenchido = 0;
-    for(aresta_index in arestas){
+    for(var aresta_index in arestas){
         var aresta = arestas[aresta_index];
         if(this.arestaMarcada(aresta)){
             countPreenchido++;
@@ -160,7 +180,7 @@ Tabuleiro.prototype.temMarcarQuartaLinha = function(){
 		for (var j=0;j<this.colunasQuadrados;j++){
 			var arestas = this.getQuadradoArestas(j, i);
 			var countPreenchido = 0;
-			for(aresta_index in arestas){
+			for(var aresta_index in arestas){
 				var aresta = arestas[aresta_index];
 				if(this.arestaMarcada(aresta)){
 					countPreenchido++;
@@ -182,7 +202,7 @@ Tabuleiro.prototype.temMarcarTerceiraLinha = function(){
         for (var j=0;j<this.colunasQuadrados;j++){            
             var arestas = this.getQuadradoArestas(j, i);            
             var countPreenchido = 0;
-            for(aresta_index in arestas){
+            for(var aresta_index in arestas){
                 var aresta = arestas[aresta_index];
                 if(this.arestaMarcada(aresta)){
                     countPreenchido++;
@@ -204,7 +224,7 @@ Tabuleiro.prototype.temMarcarTerceiraLinha = function(){
 Tabuleiro.prototype.podeFechar = function(cordX, cordY) {
     var arestas = this.getQuadradoArestas(cordX, cordY);
     var countPreenchido = 0;
-    for(aresta_index in arestas){
+    for(var aresta_index in arestas){
         var aresta = arestas[aresta_index];
         if(this.arestaMarcada(aresta)){
             countPreenchido++;
@@ -223,7 +243,7 @@ Tabuleiro.prototype.temMarcarPrimeiraOuSegundaLinha = function() {
         for (var j=0;j<this.colunasQuadrados;j++){            
             var arestas = this.getQuadradoArestas(j, i);            
             var countPreenchido = 0;
-            for(aresta_index in arestas){
+            for(var aresta_index in arestas){
                 var aresta = arestas[aresta_index];
                 if(this.arestaMarcada(aresta)){
                     countPreenchido++;
@@ -239,25 +259,80 @@ Tabuleiro.prototype.temMarcarPrimeiraOuSegundaLinha = function() {
 
 /**
 * Marca terceira linha no quadrado que foi passado por parametro em qualquer uma das 2 arestas que sao possiveis
+* @param cordX Coordenada x de um quadrado
+* @param cordY Coordenada y de um quadrado
+* @param player "jogador" ou "computador"
 * @return Retorna true se conseguiu marcar uma terceira linha do quadrado parametro.
  */
 Tabuleiro.prototype.marcaTerceiraLinha = function(cordX, cordY, player) {
     var arestas = this.getQuadradoArestas(cordX, cordY);
     var countPreenchido = 0;
-    var marcarEstaAresta;
-    for(aresta_index in arestas){
+    var arestaAmarcar;
+    for(var aresta_index in arestas){
         var aresta = arestas[aresta_index];
         if(this.arestaMarcada(aresta)){
             countPreenchido++;            
         } else {
-            marcarEstaAresta = aresta;
+            arestaAmarcar = aresta;
         }
     }
     if (countPreenchido==2) {
-        this.marcaArestas(marcarEstaAresta,player);
+        this.marcaArestas(arestaAmarcar,player);
         return true;        
     }
     return false;
 }
 
+/**
+ * Marca no tabuleiro qualquer uma linha que seja a quarta linha de um quadrado.
+ * @return Retorna o outro quadrado que essa linha tambem influencia. 
+ * Caso essa linha nao influencie nenhum outro quadrado retorne null
+ */
+Tabuleiro.prototype.marcaQualquerQuartaLinha = function() {
+    
+}
+ 
+
+
+/**
+* Marca no tabuleiro a quarta linha do quadrado passado por parametro
+* @param cordX Coordenada x do quadrado
+* @param coordY Coordenada y do quadrado
+* @player "jogador" ou "computador" 
+* @return Retorna um vetor que contem: na primeira posição o outro quadrado que essa 4a linha feita tambem influencia e
+*  na segunda posição a quantidade de quadrados simultaneamente fechados ao se fazer essa quarta linha (pode ser 1 ou 2)
+* Retorna null caso nao consiga marcar a quarta linha no quadrado passado por parametro.
+ */
+Tabuleiro.prototype.marcaQuartaLinha = function(cordX, cordY, player) {
+    var arestas = this.getQuadradoArestas(cordX, cordY);
+    var countPreenchido = 0;
+    var arestaAmarcar = undefined;
+    for(var aresta_index in arestas){
+        var aresta = arestas[aresta_index];
+        if(this.arestaMarcada(aresta)){
+            countPreenchido++;            
+        } else {
+            arestaAmarcar = aresta;
+        }
+    }
+    if (countPreenchido==3 && arestaAmarcar != undefined) {        
+        var ret = {};
+        ret.quadradoInfluenciado = undefined;
+        ret.qtdFechadosSimultaneamente = 1;
+        var quadradosDaAresta = this.getArestaQuadrados(arestaAmarcar);
+        if (quadradosDaAresta.length > 1) { // Espera-se que quando quadrados.length > 1, quadrados possua aresta interna (ou seja, as que nao circundam o tabuleiro) que é o que queremos testar.
+            for (var i in quadradosDaAresta) {
+                if (!isEqual(quadradosDaAresta[i],[cordX, cordY])) {
+                    ret.quadradoInfluenciado = quadradosDaAresta[i];
+                    if (this.podeFechar(ret.quadradoInfluenciado[0],ret.quadradoInfluenciado[1])) {
+                        ret.qtdFechadosSimultaneamente = 2;                        
+                    }               
+               }
+            }            
+            this.marcaArestas(arestaAmarcar,player);
+        }                      
+        return ret;
+    }
+    return null;    
+}
 
