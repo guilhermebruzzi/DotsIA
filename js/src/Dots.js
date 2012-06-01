b96cadcc87657d91aa34c12874020928671195cd
unknown <Guilherme@Guilherme-PC.(none)>
2012-06-01 05:19:02 -0300

    Desenhando os fundos dos quadrados

--- js/src/Dots.js
+++ js/src/Dots.js
@@ -1,7 +1,53 @@
+function geraMalhaQuadrados(malhaQuadrados, _linhas, _colunas){
+	var linhasQuadrados = _linhas - 1,
+		colunasQuadrados = _colunas - 1,
+		html = "";
+	for(var y = 0; y < linhasQuadrados; y++){
+		for(var x = 0; x < colunasQuadrados; x++){
+			var chave = x + "" + y;
+			html += "<div id='fundo-quadrado" + chave + "' class='fundo-quadrado'></div>";
+		}
+	}
+	malhaQuadrados.html(html);
+}
+
 function Dots(_linhas, _colunas){
 	this.agente = new Agente();
 	this.tabuleiro = new Tabuleiro(_linhas, _colunas);
+	this.malhaDom = $("#malha");
+	this.malhaQuadrados = $("#malha-quadrados");
+	geraMalhaQuadrados(this.malhaQuadrados, _linhas, _colunas);
+	this.malhaQuadrados.height( (_linhas-1) * 40 + _linhas * 16 );
+	this.malhaQuadrados.width( (_colunas-1) * 40 + _colunas * 16 );
 }
+
+Dots.prototype.pintaFundoQuadrado = function(x, y, classe, innerHtml){ // classe == "fundo-quadrado-jogador" ou classe == "fundo-quadrado-pc"
+	var chave = x + "" + y;
+	var id = "#fundo-quadrado" + chave;
+	var quadradoDom = $(id);
+	if(!quadradoDom.hasClass(classe)){
+		quadradoDom.addClass(classe);
+		quadradoDom.html(innerHtml);
+	}
+};
+
+Dots.prototype.pintaFundoQuadrados = function(){
+	for (quadradoIndex in this.tabuleiro.quadradosJogador){
+		var quadrado = this.tabuleiro.quadradosJogador[quadradoIndex];
+		var x = quadrado[0];
+		var y = quadrado[1];
+		var innerHtml = "<p>JO</p>";
+		this.pintaFundoQuadrado(x, y, "fundo-quadrado-jogador", innerHtml);
+	}
+	
+	for (quadradoIndex in this.tabuleiro.quadradosComputador){
+		var quadrado = this.tabuleiro.quadradosComputador[quadradoIndex];
+		var x = quadrado[0];
+		var y = quadrado[1];
+		var innerHtml = "<p>PC</p>";
+		this.pintaFundoQuadrado(x, y, "fundo-quadrado-pc", innerHtml);
+	}
+};
 
 Dots.prototype.getHtmlElemento = function(classe, indice){
 	var template = '<div class="%class%" data-indice="%indice%"></div>';
@@ -31,7 +77,8 @@ Dots.prototype.getHtmlVertice = function(first){
 Dots.prototype.getHtmlPlacar = function(){
     var placar_pc = this.tabuleiro.quadradosComputador.length;
     var placar_jogador = this.tabuleiro.quadradosJogador.length;
-    var html = "Placar: pc " + placar_pc + " x " + placar_jogador + " jogador<br />";
+    var html = "<b>Placar:</b> pc " + placar_pc + " x " + placar_jogador + " jogador<br />";
+	var resultadoFinal = "</p><br />";
     if(placar_pc + placar_jogador == this.tabuleiro.linhasQuadrados * this.tabuleiro.colunasQuadrados){
         var resultado = "<span class='empate'>Deu empate!</span>";
         if(placar_pc != placar_jogador){
@@ -42,15 +89,17 @@ Dots.prototype.getHtmlPlacar = function(){
                 resultado = "<span class='vencedor'>Vencedor: PC! :) </span>&nbsp;<span class='perdedor'>Perdedor: jogador! :( </span>";
             }
         }
-        html += "<b class='acabou'>ACABOU!</b>&nbsp;" + resultado + "</p><br />";
+        resultadoFinal = "<b class='acabou'>ACABOU!</b>&nbsp;" + resultado + "</p><br />";
     }
+	html += resultadoFinal;
     return html;
 }
 
 Dots.prototype.getHtmlMalha = function(){
-	var html = "<p>Malha " + this.tabuleiro.linhas + " x " + this.tabuleiro.colunas + " <br />";
+	var html = "<p class='resultado-tabuleiro'><b>Malha:</b> " + this.tabuleiro.linhas + " x " + this.tabuleiro.colunas + " <br />";
 	html += this.getHtmlPlacar();
 	var linha = 1;
+	html += " <div class='malha-escrita'> ";
 	for(var i = 0; i < this.tabuleiro.linhas; i++){
 		if(i > 0){
 			for(var j = 0; j < this.tabuleiro.colunas; j++){
@@ -69,11 +118,13 @@ Dots.prototype.getHtmlMalha = function(){
 			}
 		}
 	}
+	html += " </div> ";
 	return html;
 }
 
 Dots.prototype.atualizaMalha = function(){
-	$("#malha").html(this.getHtmlMalha());
+	this.malhaDom.html(this.getHtmlMalha());
+	this.pintaFundoQuadrados(); // Atualiza Fundos
 }
 
 Dots.prototype.trocaVez = function(){
